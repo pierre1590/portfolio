71f4d41fc933a5202ff0ebe8efe40002d48881c8
pierre1590 <piero.sa@icloud.com>
2018-02-19 19:07:37 +0100

    Add dependencies

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 var express = require('express');
-var app = require('express');
+var app = express();
+
+
+var port = 8080 //process.env.PORT;
+app.listen(port, function(err){
+    console.log('The server is running on port:  '+ port);
+});
