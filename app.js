e464c7a64ae058f131a34f4a6dac7a240440926c
pierre1590 <piero.sa@icloud.com>
2018-02-19 19:18:45 +0100

    Express app

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ var app = express();
 
 
 var port = 8080 //process.env.PORT;
+
+app.get('/', function(req, res){
+    res.send("Hello world!");
+});
+
 app.listen(port, function(err){
     console.log('The server is running on port:  '+ port);
 });
